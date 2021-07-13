9ca7ae9f509df865f33b4802a649616efd646be3
Rachel Solov <rsolov23@yahoo.com>
2021-07-13 12:56:02 -0500

    comments

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,16 +1,23 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   getAllPizza,
   getPizzaById,
   createPizza,
   updatePizza,
-  deletePizza,
-} = require("../../controllers/pizza-controller");
+  deletePizza
+} = require('../../controllers/pizza-controller');
 
-//set up GET all and POST at /api/pizza
-router.route("/").get(getAllPizza).post(createPizza);
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
 
-//set up GET one, PUT, and delete at /api/pizzas/:id
-router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-app.use(require("./routes"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// use this to log mongo queries being executed
+
+// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
+
+app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
