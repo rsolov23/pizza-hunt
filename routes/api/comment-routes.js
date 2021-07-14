088434a2faf9b4042eeea020ea54e962d25f3798
Rachel Solov <rsolov23@yahoo.com>
2021-07-14 08:58:37 -0500

    reply

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
 router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
+//DELETE route to handle removeReply. You'll need to create a new route for this one, because you'll need the id of the individual reply, not just its parent.
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 module.exports = router;
